9344ab310523c34b7b6531ca09c2173333db3077
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-03 20:44:48 +0600

    mutex

---
+++ 02-mutex/go.mod
@@ -0,0 +1,3 @@
+module github.com/OlyMahmudMugdho/golang-multithreading-concurrency/mutex
+
+go 1.23.2
---
+++ 02-mutex/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	var counter int = 0
+
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			increment(&counter, &mu)
+		}()
+	}
+
+	wg.Wait()
+
+	/*
+		this process will not work perfectly
+		var wg sync.WaitGroup
+
+		for i := 0; i < 10000; i++ {
+				this process will not work perfectly
+				 wg.Add(1)
+				go increment(&counter, &wg)
+			}
+
+		this process will not work perfectly
+		 wg.Wait()
+	*/
+
+	fmt.Println(counter)
+}
+
+/*
+func increment(value *int, wg *sync.WaitGroup) {
+		this process will not work perfectly
+		defer wg.Done()
+		// *value = *value + 1
+	}
+*/
+
+func increment(value *int, mu *sync.Mutex) {
+	mu.Lock()
+	*value = *value + 1
+	defer mu.Unlock()
+}
